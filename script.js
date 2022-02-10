a0175881f00382ddb3e9a9bc17b1c83e4e52f59b
sdfg9171 <87844076+sdfg9171@users.noreply.github.com>
2022-02-10 14:36:48 +0900

    Javascript: user click
    if user click on the add button

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ inputbox.onkeyup = () =>{/*onkeyup 키보드를 떼었을때*/
     }
 
 }
+
+//if user click on the add button
+addBtn.onclick = () =>{
+    let userData = inputbox.value;//getting user entered value
+    let getLocalStorage = localStorage.getItem("New Todo");//key로 부터 데이터 읽기
+    if(getLocalStorage == null){//if localStorage is null
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage);//string 객체를 json 객체로 변환
+    }
+    listArr.push(userData);
+    localStorage.setItem("New Todo",JSON.stringify(listArr));//key에 데이터(value) 쓰기  /     stringify 메소드는 json 객체를 String 객체로 변환시켜 줍니다.
+
+}
